7f3304b6a3fdcd8c538c4ccc620f023942a9d9c0
Henry Zheng <henry.zheng727@gmail.com>
2026-05-09 13:08:06 -0600

    add Env::new()

--- src/interpreter/treewalk/types.rs
+++ src/interpreter/treewalk/types.rs
@@ -3,31 +3,36 @@ use std::{collections::HashMap, rc::Rc};
 use crate::frontend::parser::ast::{Binding, Expr, Stmt, Type};
 
 #[derive(Debug)]
-pub(super) struct Env {
-    pub next: Option<Rc<Env>>,
-    pub binding: (String, Rc<Value>),
+pub(super) enum Env {
+    Mt, // empy :3
+    Cons {
+        next: Rc<Env>,
+        binding: (String, Rc<Value>),
+    },
 }
 
 impl Env {
     pub(super) fn new() -> Env {
-        todo!()
+        Env::Mt
     }
 
     pub(super) fn extend(curr: Rc<Env>, id: String, val: Rc<Value>) -> Rc<Env> {
-        Env {
-            next: Some(curr),
+        Env::Cons {
+            next: curr,
             binding: (id, val),
         }.into()
     }
 
     pub(super) fn lookup(&self, key: &str) -> Option<Rc<Value>> {
-        if self.binding.0 == key {
-            return Some(Rc::clone(&self.binding.1));
-        }
-
-        match &self.next {
-            Some(env) => env.lookup(key),
-            None => None,
+        match self {
+            Env::Mt => None,
+            Env::Cons { next, binding } => {
+                if binding.0 == key {
+                    Some(Rc::clone(&binding.1))
+                } else {
+                    next.lookup(key)
+                }
+            }
         }
     }
 }
