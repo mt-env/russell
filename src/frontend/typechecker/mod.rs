48fcd7d31aa7fd7a96cab503bdb17d6b3e6050a4
Henry Zheng <henry.zheng727@gmail.com>
2026-05-12 16:22:32 -0600

    use trees-that-grow strategy in typechecker

--- src/frontend/typechecker/mod.rs
+++ src/frontend/typechecker/mod.rs
@@ -1,10 +1,10 @@
-use crate::frontend::parser::ast::Defn;
+use crate::frontend::{parser::ast::ParsedDefn, typechecker::types::TypedDefn};
 
 pub mod types;
 pub mod typecheck_expr;
 pub mod typecheck_fn;
 
 
-fn typecheck(defns: Vec<Defn>) -> Vec<TypedDefn> {
+fn typecheck(defns: Vec<ParsedDefn>) -> Vec<TypedDefn> {
     todo!()
 }
