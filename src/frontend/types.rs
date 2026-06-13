92b73b8b6bc0afbe28dc352ada8aeb734ff84d24
Henry Zheng <henry.zheng727@gmail.com>
2026-06-13 17:28:18 -0600

    make spanned impl display

--- src/frontend/types.rs
+++ src/frontend/types.rs
@@ -1,5 +1,13 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Spanned<T> {
     pub offset: usize,
     pub node: T,
+}
+
+impl<T: Display> Display for Spanned<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.node)
+    }
 }
