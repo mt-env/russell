279ec4f7567a7f58880c76b8557fddb549f8cd34
Henry Zheng <henry.zheng727@gmail.com>
2026-06-20 13:35:27 -0600

    add more tests

--- src/frontend/typechecker/typecheck_expr/tests.rs
+++ src/frontend/typechecker/typecheck_expr/tests.rs
@@ -55,3 +55,98 @@ fn test_add() {
         )
     );
 }
+
+#[test]
+fn test_nested() {
+    let expr = typeck("1 + 2 * 3");
+    assert_eq!(
+        expr,
+        TypedExpr::new(
+            0,
+            TypeValue::Int,
+            ExprKind::Plus(
+                Box::new(TypedExpr::new(0, TypeValue::Int, ExprKind::Int(1))),
+                Box::new(TypedExpr::new(
+                    4,
+                    TypeValue::Int,
+                    ExprKind::Mult(
+                        Box::new(TypedExpr::new(4, TypeValue::Int, ExprKind::Int(2))),
+                        Box::new(TypedExpr::new(8, TypeValue::Int, ExprKind::Int(3)))
+                    )
+                ))
+            )
+        )
+    );
+}
+
+#[test]
+fn test_parens() {
+    let expr = typeck("(1 + 2) * 3");
+    assert_eq!(
+        expr,
+        TypedExpr::new(
+            0,
+            TypeValue::Int,
+            ExprKind::Mult(
+                Box::new(TypedExpr::new(
+                    0,
+                    TypeValue::Int,
+                    ExprKind::Plus(
+                        Box::new(TypedExpr::new(1, TypeValue::Int, ExprKind::Int(1))),
+                        Box::new(TypedExpr::new(5, TypeValue::Int, ExprKind::Int(2)))
+                    )
+                )),
+                Box::new(TypedExpr::new(10, TypeValue::Int, ExprKind::Int(3)))
+            )
+        )
+    );
+}
+
+#[test]
+fn test_relational() {
+    let expr = typeck("1 < 2");
+    assert_eq!(
+        expr,
+        TypedExpr::new(
+            0,
+            TypeValue::Bool,
+            ExprKind::Less(
+                Box::new(TypedExpr::new(0, TypeValue::Int, ExprKind::Int(1))),
+                Box::new(TypedExpr::new(4, TypeValue::Int, ExprKind::Int(2)))
+            )
+        )
+    );
+}
+
+#[test]
+fn test_logical() {
+    let expr = typeck("true && false");
+    assert_eq!(
+        expr,
+        TypedExpr::new(
+            0,
+            TypeValue::Bool,
+            ExprKind::And(
+                Box::new(TypedExpr::new(0, TypeValue::Bool, ExprKind::Bool(true))),
+                Box::new(TypedExpr::new(8, TypeValue::Bool, ExprKind::Bool(false)))
+            )
+        )
+    );
+}
+
+#[test]
+fn test_if() {
+    let expr = typeck("if true then 1 else 2");
+    assert_eq!(
+        expr,
+        TypedExpr::new(
+            0,
+            TypeValue::Int,
+            ExprKind::If(
+                Box::new(TypedExpr::new(3, TypeValue::Bool, ExprKind::Bool(true))),
+                Box::new(TypedExpr::new(13, TypeValue::Int, ExprKind::Int(1))),
+                Box::new(TypedExpr::new(20, TypeValue::Int, ExprKind::Int(2)))
+            )
+        )
+    );
+}
