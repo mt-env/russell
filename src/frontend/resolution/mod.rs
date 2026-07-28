fb2576b7870a58e9e0efa71d090f6665500de249
Henry Zheng <henry.zheng727@gmail.com>
2026-07-28 16:41:20 -0600

    scaffold ast for name resolution

---
+++ src/frontend/resolution/mod.rs
@@ -0,0 +1,7 @@
+use crate::frontend::parser::ast::ParsedDefn;
+
+pub mod types;
+
+pub fn resolve(defns: Vec<ParsedDefn>) {
+    todo!()
+}
---
+++ src/frontend/resolution/types.rs
@@ -0,0 +1,83 @@
+pub struct TypeId(usize);
+pub struct TypeParam(usize);
+pub struct ValueId(usize);
+
+pub enum ResolvedDefn {
+    Typedef {},
+
+    Fn {
+        id: ValueId,
+        ret_ty: ResolvedType,
+        body: Vec<ResolvedStmt>,
+    },
+}
+
+pub enum ResolvedStmt {
+    Let(ValueId, ResolvedExpr),
+    Read(ValueId, ResolvedType),
+    Echo(ResolvedType, ResolvedExpr),
+    Return(ResolvedExpr),
+}
+
+pub enum ResolvedExpr {
+    Int(i64),
+    Bool(bool),
+    Float(f64),
+
+    Id(ValueId),
+
+    Fn(ResolvedBinding, Box<ResolvedExpr>),
+
+    Neg(Box<ResolvedExpr>),
+    FNeg(Box<ResolvedExpr>),
+    Bang(Box<ResolvedExpr>),
+
+    Call(Box<ResolvedExpr>, Box<ResolvedExpr>),
+
+    Plus(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Minus(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FPlus(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FMinus(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Mult(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FMult(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Div(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FDiv(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Pipe(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Lt(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Gt(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    LtEq(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    GtEq(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FLt(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FGt(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FLtEq(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    FGtEq(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Eq(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    NotEq(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Or(Box<ResolvedExpr>, Box<ResolvedExpr>),
+    And(Box<ResolvedExpr>, Box<ResolvedExpr>),
+
+    If(Box<ResolvedExpr>, Box<ResolvedExpr>, Box<ResolvedExpr>),
+    Match(Box<ResolvedExpr>, Vec<(ResolvedExpr, ResolvedExpr)>),
+}
+
+pub enum ResolvedType {
+    Int,
+    Float,
+    Bool,
+
+    TypeId(TypeId),
+    TypeParam(TypeParam),
+
+    Fn(Box<ResolvedType>, Box<ResolvedType>),
+}
+
+pub struct ResolvedBinding {
+    id: ValueId,
+    typ: ResolvedType,
+}
+
+pub struct ResolvedMatchArm {
+    id: ValueId,
+    bindings: Vec<ValueId>,
+    expr: ResolvedExpr,
+}
