a8f46819c18f9a1640d0b1820643f6c9d54c08cb
Henry Zheng <henry.zheng727@gmail.com>
2026-07-30 14:54:13 -0600

    add first attempt at scope management for name resolution

--- src/frontend/resolution/types.rs
+++ src/frontend/resolution/types.rs
@@ -1,9 +1,15 @@
 use std::collections::HashMap;
 
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub struct TypeId(usize);
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub struct TypeParamId(usize);
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub struct ValueId(usize);
 
+#[derive(Copy, Clone, Eq, Hash, PartialEq)]
 pub enum Identifier {
     TypeId(TypeId),
     TypeParamId(TypeParamId),
@@ -101,38 +107,75 @@ pub struct ResolverCtx<'a> {
     num_typeparamids: usize,
     num_valueids: usize,
 
+    ids: HashMap<Identifier, &'a str>,
+
     env: Vec<Env<'a>>,
+}
+
+impl<'a> ResolverCtx<'a> {
+    pub(crate) fn new() -> Self {
+        let mut env = Vec::new();
+        env.push(Env::new());
+        Self {
+            num_typeids: 0,
+            num_typeparamids: 0,
+            num_valueids: 0,
+            ids: HashMap::new(),
+            env,
+        }
+    }
+
+    pub(crate) fn next_typeid(&mut self) -> TypeId {
+        let id = TypeId(self.num_typeids);
+        self.num_typeids += 1;
+        id
+    }
+
+    pub(crate) fn next_typeparamid(&mut self) -> TypeParamId {
+        let id = TypeParamId(self.num_typeparamids);
+        self.num_typeparamids += 1;
+        id
+    }
+
+    pub(crate) fn next_valueid(&mut self) -> ValueId {
+        let id = ValueId(self.num_valueids);
+        self.num_valueids += 1;
+        id
+    }
+
+    pub(crate) fn add_value(&mut self, name: &'a str) -> ValueId {
+        let id = self.next_valueid();
+        self.ids.insert(Identifier::ValueId(id), name);
+        // TODO - add it to the environment
+        id
+    }
+
+    pub(crate) fn push_scope(&mut self) {
+        self.env.push(Env::new());
+    }
+
+    pub(crate) fn pop_scope(&mut self) {
+        self.env.pop();
+    }
+
+    pub(crate) fn lookup(&self, name: &str) -> Option<Identifier> {
+        for env in self.env.iter().rev() {
+            if let Some(id) = env.values.get(name) {
+                return Some(*id);
+            }
+        }
+        None
+    }
 }
 
 pub struct Env<'a> {
     values: HashMap<&'a str, Identifier>,
 }
 
-impl<'a> ResolverCtx<'a> {
+impl<'a> Env<'a> {
     pub fn new() -> Self {
         Self {
-            num_typeids: 0,
-            num_typeparamids: 0,
-            num_valueids: 0,
-            env: Vec::new(),
+            values: HashMap::new(),
         }
-    }
-
-    pub fn next_typeid(&mut self) -> TypeId {
-        let id = TypeId(self.num_typeids);
-        self.num_typeids += 1;
-        id
-    }
-
-    pub fn next_typeparamid(&mut self) -> TypeParamId {
-        let id = TypeParamId(self.num_typeparamids);
-        self.num_typeparamids += 1;
-        id
-    }
-
-    pub fn next_valueid(&mut self) -> ValueId {
-        let id = ValueId(self.num_valueids);
-        self.num_valueids += 1;
-        id
     }
 }
