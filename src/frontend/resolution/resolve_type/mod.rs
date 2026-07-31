749b8f7a37421200f34d4f010eeeb32b84d7b65f
Henry Zheng <henry.zheng727@gmail.com>
2026-07-31 16:33:44 -0600

    resolve primitive types

--- src/frontend/resolution/resolve_type/mod.rs
+++ src/frontend/resolution/resolve_type/mod.rs
@@ -4,7 +4,15 @@ use crate::frontend::{
 };
 
 pub fn resolve_type(ctx: &mut ResolverCtx, ty: Type) -> ResolvedType {
-    todo!()
+    match ty {
+        Type::Int => ResolvedType::Int,
+        Type::Float => ResolvedType::Float,
+        Type::Bool => ResolvedType::Bool,
+        Type::TypeId(_) => todo!(),
+        Type::TypeParam(_) => todo!(),
+        Type::TypeApp(_, items) => todo!(),
+        Type::Fn(domain, codomain) => todo!(),
+    }
 }
 
 pub fn resolve_binding<'a>(
