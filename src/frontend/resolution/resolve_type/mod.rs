e2f6c5c77f83e3ab3ac8c8592cc30d249d8b2dfa
Henry Zheng <henry.zheng727@gmail.com>
2026-07-31 17:25:38 -0600

    add resolution for type applications

--- src/frontend/resolution/resolve_type/mod.rs
+++ src/frontend/resolution/resolve_type/mod.rs
@@ -1,6 +1,6 @@
 use crate::frontend::{
     parser::ast::{ParsedBinding, Type},
-    resolution::types::{ResolvedBinding, ResolvedType, ResolverCtx},
+    resolution::types::{ResolvedBinding, ResolvedType, ResolverCtx, TypeId, TypeParamId},
 };
 
 pub fn resolve_type<'a>(ctx: &mut ResolverCtx<'a>, ty: Type<'a>) -> ResolvedType {
@@ -8,25 +8,38 @@ pub fn resolve_type<'a>(ctx: &mut ResolverCtx<'a>, ty: Type<'a>) -> ResolvedType
         Type::Int => ResolvedType::Int,
         Type::Float => ResolvedType::Float,
         Type::Bool => ResolvedType::Bool,
-        Type::TypeId(name) => resolve_typeid(ctx, name),
-        Type::TypeParam(name) => resolve_typeparam(ctx, name),
-        Type::TypeApp(_, items) => todo!(),
+        Type::TypeId(name) => ResolvedType::TypeId(resolve_typeid(ctx, name)),
+        Type::TypeParam(name) => ResolvedType::TypeParam(resolve_typeparam(ctx, name)),
+        Type::TypeApp(type_constr, type_args) => resolve_typeapp(ctx, type_constr, type_args),
         Type::Fn(domain, codomain) => resolve_fn(ctx, *domain, *codomain),
     }
 }
 
-fn resolve_typeid(ctx: &ResolverCtx, name: &str) -> ResolvedType {
-    let Some(id) = ctx.lookup_type(name) else {
-        panic!("Unbound type: {}", name);
-    };
-    ResolvedType::TypeId(id)
+fn resolve_typeid(ctx: &ResolverCtx, name: &str) -> TypeId {
+    match ctx.lookup_type(name) {
+        Some(val) => val,
+        None => panic!("TypeId {} not found in context", name),
+    }
 }
 
-fn resolve_typeparam<'a>(ctx: &mut ResolverCtx<'a>, name: &'a str) -> ResolvedType {
+fn resolve_typeparam<'a>(ctx: &mut ResolverCtx<'a>, name: &'a str) -> TypeParamId {
     match ctx.lookup_typeparam(name) {
-        Some(val) => ResolvedType::TypeParam(val),
-        None => ResolvedType::TypeParam(ctx.add_typeparam(name)),
+        Some(val) => val,
+        None => ctx.add_typeparam(name),
     }
+}
+
+fn resolve_typeapp<'a>(
+    ctx: &mut ResolverCtx<'a>,
+    type_constr: &'a str,
+    type_args: Vec<Type<'a>>,
+) -> ResolvedType {
+    let resolved_type_constr = resolve_typeid(ctx, type_constr);
+    let mut resolved_type_args = Vec::with_capacity(type_args.len());
+    for arg in type_args {
+        resolved_type_args.push(resolve_type(ctx, arg));
+    }
+    ResolvedType::TypeApp(resolved_type_constr, resolved_type_args)
 }
 
 fn resolve_fn<'a>(ctx: &mut ResolverCtx<'a>, domain: Type<'a>, codomain: Type<'a>) -> ResolvedType {
