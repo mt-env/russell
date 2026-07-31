b0ee9129d49feba60f114c6042bbf64a4be0c42c
Henry Zheng <henry.zheng727@gmail.com>
2026-07-31 15:35:42 -0600

    initialize names of all defns/funcs before processing them
    this allows mutually recursive types and functions

--- src/frontend/resolution/mod.rs
+++ src/frontend/resolution/mod.rs
@@ -1,4 +1,7 @@
-use crate::frontend::parser::ast::ParsedDefn;
+use crate::frontend::{
+    parser::ast::{Defn, ParsedDefn},
+    resolution::types::ResolverCtx,
+};
 
 pub mod resolve_defn;
 pub mod resolve_expr;
@@ -8,4 +11,18 @@ pub mod types;
 
 pub fn resolve(defns: Vec<ParsedDefn>) {
     todo!()
+}
+
+pub fn init_global_scope<'a>(ctx: &mut ResolverCtx<'a>, defns: &[ParsedDefn<'a>]) {
+    // TODO - intialize global types for int, float, bool?
+    for defn in defns {
+        match defn.node {
+            Defn::Typedef { id, .. } => {
+                ctx.add_type(id);
+            }
+            Defn::Fn { name, .. } => {
+                ctx.add_value(name);
+            }
+        }
+    }
 }
