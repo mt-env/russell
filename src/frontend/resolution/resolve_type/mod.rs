3cdbab4fecaa38715ad3444dbad52355aa9c9a57
Henry Zheng <henry.zheng727@gmail.com>
2026-07-31 17:19:14 -0600

    add resolution for types and type params

--- src/frontend/resolution/resolve_type/mod.rs
+++ src/frontend/resolution/resolve_type/mod.rs
@@ -3,15 +3,29 @@ use crate::frontend::{
     resolution::types::{ResolvedBinding, ResolvedType, ResolverCtx},
 };
 
-pub fn resolve_type(ctx: &mut ResolverCtx, ty: Type) -> ResolvedType {
+pub fn resolve_type<'a>(ctx: &mut ResolverCtx<'a>, ty: Type<'a>) -> ResolvedType {
     match ty {
         Type::Int => ResolvedType::Int,
         Type::Float => ResolvedType::Float,
         Type::Bool => ResolvedType::Bool,
-        Type::TypeId(_) => todo!(),
-        Type::TypeParam(_) => todo!(),
+        Type::TypeId(name) => resolve_typeid(ctx, name),
+        Type::TypeParam(name) => resolve_typeparam(ctx, name),
         Type::TypeApp(_, items) => todo!(),
         Type::Fn(domain, codomain) => todo!(),
+    }
+}
+
+fn resolve_typeid(ctx: &ResolverCtx, name: &str) -> ResolvedType {
+    let Some(id) = ctx.lookup_type(name) else {
+        panic!("Unbound type: {}", name);
+    };
+    ResolvedType::TypeId(id)
+}
+
+fn resolve_typeparam<'a>(ctx: &mut ResolverCtx<'a>, name: &'a str) -> ResolvedType {
+    match ctx.lookup_typeparam(name) {
+        Some(val) => ResolvedType::TypeParam(val),
+        None => ResolvedType::TypeParam(ctx.add_typeparam(name)),
     }
 }
 
