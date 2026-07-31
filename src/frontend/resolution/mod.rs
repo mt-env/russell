826af48d8298f8d7c00ce933371b42d36db76618
Henry Zheng <henry.zheng727@gmail.com>
2026-07-31 16:14:45 -0600

    add constructors to global scope too

--- src/frontend/resolution/mod.rs
+++ src/frontend/resolution/mod.rs
@@ -10,15 +10,26 @@ pub mod resolve_type;
 pub mod types;
 
 pub fn resolve(defns: Vec<ParsedDefn>) {
-    todo!()
+    let mut ctx = ResolverCtx::new();
+    init_global_scope(&mut ctx, &defns);
+    for defn in defns {
+        resolve_defn::resolve_defn(&mut ctx, defn);
+    }
 }
 
 pub fn init_global_scope<'a>(ctx: &mut ResolverCtx<'a>, defns: &[ParsedDefn<'a>]) {
     // TODO - intialize global types for int, float, bool?
     for defn in defns {
-        match defn.node {
-            Defn::Typedef { id, .. } => {
+        match &defn.node {
+            Defn::Typedef {
+                id,
+                ty_vars: _,
+                arms,
+            } => {
                 ctx.add_type(id);
+                for arm in arms {
+                    ctx.add_value(arm.0);
+                }
             }
             Defn::Fn { name, .. } => {
                 ctx.add_value(name);
